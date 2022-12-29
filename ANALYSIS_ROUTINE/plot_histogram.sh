26fa2f8326b678f204abf3b6dac8042f428b87e0
Nai-Chieh Lin <daniellin456@gmail.com>
2022-12-29 10:33:46 +0800

    <fix> file path

--- ANALYSIS_ROUTINE/plot_histogram.sh
+++ ANALYSIS_ROUTINE/plot_histogram.sh
@@ -8,11 +8,11 @@ figure_output="/data/daniellin/RAMSES_RUNS/EOS_GAMMA_HEATING_COOLING/TR_FLOOR_10
 source /home/ynlee/softs/envpym
 
 # Modify the G0 factor in calculate_theoretical_temperature file and Store the nH and equilibrium_temp
-sed -i '5s/'$original_G0_factor'/'$modified_G0_factor'/' /data/daniellin/COOLING_FUNCTION_ANALYZE/EQUILIBRIUM/equilibrium_temp.py
-python3 /data/daniellin/COOLING_FUNCTION_ANALYZE/EQUILIBRIUM/equilibrium_temp.py
+sed -i '5s/'$original_G0_factor'/'$modified_G0_factor'/' /data/daniellin/PYTHON_SCRIPS/COOLING_FUNCTION_ANALYZE/EQUILIBRIUM/equilibrium_temp.py
+python3 /data/daniellin/PYTHON_SCRIPS/COOLING_FUNCTION_ANALYZE/EQUILIBRIUM/equilibrium_temp.py
 
 python write_pymsesrc.py $ramses_output
-#python produce_maps.py $ramses_output
+python produce_maps.py $ramses_output
 python histogram.py -i $ramses_output -o $figure_output
 
 
