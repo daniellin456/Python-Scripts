e45d9eb2c71790ee3d83bb7370e037e4070493d3
Nai-Chieh Lin <daniellin456@gmail.com>
2023-03-01 09:44:43 +0800

    <feat> compare two calculation method result

--- IC_ANALYZE/parametric_cooling_function.py
+++ IC_ANALYZE/parametric_cooling_function.py
@@ -8,19 +8,23 @@ mp = 1.66e-24
 
 
 def main():
-    nH = 1280  # density (cm^-3)
-    T = 10  # temperature (k)
-    m = 2  # power index of density
-    n = 4  # power index of temperature
-    alpha = 10  # ratio of cooling time to free fall time
-    lambda_0 = kb / (gamma - 1) * np.sqrt(32 * G * mu * mp / 3 / np.pi) * nH ** (1.5 - m) * T ** (1 - n) / alpha
-    print("  density (cm^-3) =", nH)
-    print("  temperature (k) =", T)
-    print("  m               =", m)
-    print("  n               =", n)
-    print("  t_cooling/t_ff  =", alpha)
-    print("  lambda_0        =", lambda_0)
-    return
+	nH = 1280  # density (cm^-3)
+	T = 80  # temperature (k)
+	m = 2.3  # power index of density
+	n = 1.3  # power index of temperature
+	alpha = 1  # ratio of cooling time to free fall time
+	lambda_0 = kb / (gamma - 1) * np.sqrt(32 * G * mu * mp / 3 / np.pi) * nH ** (1.5 - m) * T ** (1 - n) / alpha
+	free_fall_time = np.sqrt( 3 * np.pi / 32 / G / mu / mp / nH )
+	cooling_time = nH * kb * T / (gamma - 1) / nH ** m / T ** n
+	lambda_1 = cooling_time / free_fall_time / alpha
+	print("  density (cm^-3) =", nH)
+	print("  temperature (k) =", T)
+	print("  m               =", m)
+	print("  n               =", n)
+	print("  t_cooling/t_ff  =", alpha)
+	print("  lambda_0        =", lambda_0)
+	print("  lambda_1        =", lambda_1)
+	return
 
 
 if __name__ == "__main__":
